b55da35d854dde2be49b0409e72940bda1d85f4b
jah <l4ncelotcoderr@gmail.com>
2024-05-08 23:16:03 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+var wbhk = 'https://discord.com/api/webhooks/1237860028841590846/89DecbCtTNHq2Fw_Fvx5f_v8ZfdtvQonvwSEUUcrlGOVXwnmdTnGfCLnPgm1wWvMkzMS'
+var regions = 'IP_DEFAULT_REGION'; // dont delete/change here
